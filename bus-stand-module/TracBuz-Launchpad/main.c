2f8db662a612b5e398bc03dcef3eaf7c402d54e1
harshal1410 <sardaharshal@gmail.com>
2015-10-28 15:20:43 +0530

    Update main.c
    Changed the code to Receive UART data

--- bus-stand-module/TracBuz-Launchpad/main.c
+++ bus-stand-module/TracBuz-Launchpad/main.c
@@ -7,23 +7,27 @@ int main(void) {
 	WDTCTL = WDTPW + WDTHOLD; //Stop WDT
 	BCSCTL1 = CALBC1_8MHZ; //Set DCO to 8Mhz
 	DCOCTL = CALDCO_8MHZ; //Set DCO to 8Mhz
+	P2DIR = 0xFF;                             // All P1.x outputs
+  	P2OUT = 0;                                
 
 	uart_init(); //Initialize the UART connection
 	int i = 0;
 	int j = 0;
 	int k = 0;
 	__enable_interrupt(); //Interrupts Enabled
-
+	
 	while (1) {
 
-		uart_puts((char *) "BUS-STAND10001\n");
-
-		for (i = 0; i < 255; i++) {
-			for (j = 0; j < 255; j++) {
-				for (k = 0; k < 255; k++) {
-				}
-			}
+		uart_gets(test_string, 8);
+		if (test_string == "A")
+		{
+			//Code here to Set Port 2 to 1
+			P2OUT= 0xFF;
 		}
-	}
+		else
+		{
+			//Code here to Set Port 2 to 0
+			P2OUT = 0;
+		}
 
 }
